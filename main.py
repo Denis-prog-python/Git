23953767d699171aaf4e8236981d6bc7d6c42252
Denis-prog-python <winterguru@yandex.ru>
2025-04-01 10:04:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ letter_count = Counter(char for char in text if char.isalpha())
 
 for letter, count in sorted(letter_count.items()):
     print(f"Буква '{letter}': {count} раз(а)")
+
+text = input("Введите текст: ")
+count = 0
+
+for char in text:
+    if char.isupper():  # проверяем, является ли символ заглавной буквой
+        count += 1
+
+print(f"Количество заглавных букв в тексте: {count}")
